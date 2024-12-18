3d389bc78d6346a0e727719c3938b21feb155b15
sam6-develop <sanskritim@kpmg.com>
2024-12-18 19:54:11 +0530

    changs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG=os.environ.get("DEBUG","False").lower()=="true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1").split(" ")
 
 
 # Application definition
