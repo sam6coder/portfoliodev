edd40706e1cd418e3b393f44c61dab4326a2d2d9
sam6-develop <sanskritim@kpmg.com>
2024-12-18 20:06:21 +0530

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG=os.environ.get("DEBUG","False").lower()=="true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1").split(" ")
+CSRF_TRUSTED_ORIGINS = ['https://portfoliodev-backend.onrender.com']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 
 # Application definition
